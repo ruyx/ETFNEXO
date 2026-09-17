add7448a7b8b26cca3e9fd758f1df1a63346ddc1
ETF Nexo <dev@etfnexo.com>
2026-09-17 10:50:32 +0200

    Add cache headers to force revalidation

--- next.config.mjs
+++ next.config.mjs
@@ -125,6 +125,19 @@ const nextConfig = {
       },
     ];
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=0, must-revalidate',
+          },
+        ],
+      },
+    ];
+  },
 }
 
 export default nextConfig
